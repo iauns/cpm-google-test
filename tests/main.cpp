e165dbd21f374c26fba4fca4331271b00ffbd81f
James Hughes <jhughes@sci.utah.edu>
2013-11-22 11:01:15 -0700

    Add travis CI hook.

---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(BasicTests, TestTrue)
+{
+  ASSERT_EQ(1, 1);
+  EXPECT_THROW(throw std::exception(), std::exception);
+}
+
+int main(int argc, char** argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
