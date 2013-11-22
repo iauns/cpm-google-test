104efcfd2ba69b016297b80dd0ca1ed923b6b4dc
James Hughes <jhughes@sci.utah.edu>
2013-11-22 12:05:48 -0700

    Added vim cookbook. Determined problem was an old version of CMake.

--- cloneCookBooks.sh
+++ cloneCookBooks.sh
@@ -19,4 +19,5 @@ CloneCookbook ./cookbooks/nodejs https://github.com/mdxp/nodejs-cookbook.git
 CloneCookbook ./cookbooks/git https://github.com/opscode-cookbooks/git.git 
 CloneCookbook ./cookbooks/subversion https://github.com/opscode-cookbooks/subversion.git
 CloneCookbook ./cookbooks/cmake https://github.com/phlipper/chef-cmake.git 
+CloneCookbook ./cookbooks/vim http://github.com/opscode-cookbooks/vim
 
